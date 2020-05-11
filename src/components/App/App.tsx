0ac7da3ef05a94a0257e338959cc6c2899435166
JoshDennis <joshuagardennis@gmail.com>
2020-05-11 20:44:53 +0100

    experimenting with changes to routing to allow more fluid navigation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import DashboardPage from "../../Pages/Dashboard";
 import MovesPage from "../../Pages/Moves";
 import AbilitiesPage from "../../Pages/Abilities";
 import PokedexPage from "../../Pages/Pokedex";
 import ItemsPage from "../../Pages/Items";
 
-const App = () => (
-  <Router>
+interface iProps {
+  location: Object;
+  match: Object;
+  history: Object;
+}
+
+const App = ({ location, match, history }: iProps) => {
+  useEffect(() => {
+    console.log("UPDATED");
+  }, [location]);
+
+  return (
     <Switch>
       <Route path="/items" component={ItemsPage} />
       <Route path="/pokedex" component={PokedexPage} />
@@ -16,7 +26,7 @@ const App = () => (
       <Route path="/moves" component={MovesPage} />
       <Route path="/" component={DashboardPage} />
     </Switch>
-  </Router>
-);
+  );
+};
 
-export default App;
+export default withRouter(App);
