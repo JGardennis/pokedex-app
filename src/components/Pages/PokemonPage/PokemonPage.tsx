c692fbf4a5f28189936323b61d966e86894400e2
JoshDennis <joshuagardennis@gmail.com>
2020-08-29 13:21:17 +0100

    loading spinner

--- src/components/Pages/PokemonPage/PokemonPage.tsx
+++ src/components/Pages/PokemonPage/PokemonPage.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import React, { useState } from "react";
+import Backdrop from "./components/Backdrop";
 import { Pokemon } from "../../../helpers/types";
 import { Container, Row, Col } from "react-bootstrap";
-import Backdrop from "./components/Backdrop";
+import { RouteComponentProps } from "react-router-dom";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 type PageData = RouteComponentProps<{ id: string }, any, { data: Pokemon }>;
 
-interface iState {}
+interface iState {
+  data: Pokemon | null;
+  isLoading: boolean;
+}
 
 const PokemonPage: React.SFC<PageData> = ({ location, match }: PageData) => {
+  const [state, setState] = useState<iState>({ data: null, isLoading: true });
+
+  if (state.isLoading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <>
       <Backdrop pokemonType="grass" />
